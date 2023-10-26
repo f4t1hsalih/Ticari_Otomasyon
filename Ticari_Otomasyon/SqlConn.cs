b043526e2941fe4531234e8a0f9d6c2a2d174735
f4t1hsalih <f4t1hsalih@gmail.com>
2023-10-26 12:30:42 +0300

    SQL bağlantı adresini alma sınıfı oluşturuldu

---
+++ Ticari_Otomasyon/SqlConn.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace Ticari_Otomasyon
+{
+    internal class SqlConn
+    {
+
+        class sqlconnection
+        {
+            public SqlConnection connection()
+            {
+                SqlConnection connect = new SqlConnection(@"Data Source = SALIH\SQLEXPRESS; Initial Catalog = Hastane; Integrated Security = True");
+                connect.Open();
+                return connect;
+            }
+
+        }
+    }
+}
