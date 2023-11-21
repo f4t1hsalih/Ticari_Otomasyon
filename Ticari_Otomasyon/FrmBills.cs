e000ed53bf9bcf63be8ccc5892d9491c08d73a34
f4t1hsalih <f4t1hsalih@gmail.com>
2023-11-21 13:50:28 +0300

    Faturalar formundan fatura ürünler formuna id çekme kodlandı

--- Ticari_Otomasyon/FrmBills.cs
+++ Ticari_Otomasyon/FrmBills.cs
@@ -138,5 +138,17 @@ namespace Ticari_Otomasyon
             List();
             Clean();
         }
+
+        private void gridView1_DoubleClick(object sender, EventArgs e)
+        {
+            FrmInvoiceProducts invoiceProduct = new FrmInvoiceProducts();
+            DataRow dr = gridView1.GetDataRow(gridView1.FocusedRowHandle);
+
+            if (dr != null)
+            {
+                invoiceProduct.id = dr["invoice_info_id"].ToString();
+            }
+            invoiceProduct.Show();
+        }
     }
 }
---
+++ Ticari_Otomasyon/FrmInvoiceProducts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ticari_Otomasyon
+{
+    public partial class FrmInvoiceProducts : Form
+    {
+        public FrmInvoiceProducts()
+        {
+            InitializeComponent();
+        }
+
+        public string id;
+        private void FrmInvoiceProducts_Load(object sender, EventArgs e)
+        {
+            label1.Text = id;
+        }
+    }
+}
