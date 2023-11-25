9a2bd825d84ac9643528e827b7d55e032dd49452
f4t1hsalih <f4t1hsalih@gmail.com>
2023-11-25 17:20:25 +0300

    Not kaydetme metodu kodlandı

--- Ticari_Otomasyon/FrmNotes.cs
+++ Ticari_Otomasyon/FrmNotes.cs
@@ -26,5 +26,20 @@ namespace Ticari_Otomasyon
         {
             List();
         }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into tbl_notes(date, hour, title, detail, creator, appeal) values (@p1, @p2, @p3, @p4, @p5)", con.connection());
+            cmd.Parameters.AddWithValue("@p1", mskDate.Text);
+            cmd.Parameters.AddWithValue("@p2", mskHour.Text);
+            cmd.Parameters.AddWithValue("@p3", txtTitle.Text);
+            cmd.Parameters.AddWithValue("@p4", rchDetail.Text);
+            cmd.Parameters.AddWithValue("@p5", txtCreator.Text);
+            cmd.Parameters.AddWithValue("@p1", txtAppeal.Text);
+            cmd.ExecuteNonQuery();
+            con.connection().Close();
+            MessageBox.Show("Not başarıyla kayedildi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            List();
+        }
     }
 }
