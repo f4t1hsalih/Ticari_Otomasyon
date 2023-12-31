67f118ad2d318ce0b3473c122820437cc4cc15c1
f4t1hsalih <f4t1hsalih@gmail.com>
2024-01-01 00:17:28 +0300

    Döviz Kurları eklendi

--- Ticari_Otomasyon/FrmQuickGlance.Designer.cs
+++ Ticari_Otomasyon/FrmQuickGlance.Designer.cs
@@ -30,67 +30,69 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmQuickGlance));
             this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
-            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridControlStocks = new DevExpress.XtraGrid.GridControl();
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.groupControl2 = new DevExpress.XtraEditors.GroupControl();
-            this.gridControl2 = new DevExpress.XtraGrid.GridControl();
+            this.gridControlFihrist = new DevExpress.XtraGrid.GridControl();
             this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.groupControl3 = new DevExpress.XtraEditors.GroupControl();
-            this.gridControl3 = new DevExpress.XtraGrid.GridControl();
+            this.gridControlCompMovements = new DevExpress.XtraGrid.GridControl();
             this.gridView3 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.groupControl4 = new DevExpress.XtraEditors.GroupControl();
-            this.gridControl4 = new DevExpress.XtraGrid.GridControl();
+            this.gridControlAgenda = new DevExpress.XtraGrid.GridControl();
             this.gridView4 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
             this.xtraTabPage1 = new DevExpress.XtraTab.XtraTabPage();
             this.xtraTabPage2 = new DevExpress.XtraTab.XtraTabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
             this.groupControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlStocks)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl2)).BeginInit();
             this.groupControl2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlFihrist)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl3)).BeginInit();
             this.groupControl3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlCompMovements)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl4)).BeginInit();
             this.groupControl4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlAgenda)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).BeginInit();
             this.xtraTabControl1.SuspendLayout();
+            this.xtraTabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupControl1
             // 
             this.groupControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.groupControl1.Controls.Add(this.gridControl1);
+            this.groupControl1.Controls.Add(this.gridControlStocks);
             this.groupControl1.Location = new System.Drawing.Point(12, 284);
             this.groupControl1.Name = "groupControl1";
             this.groupControl1.Size = new System.Drawing.Size(307, 265);
             this.groupControl1.TabIndex = 0;
             this.groupControl1.Text = "AZALAN STOKLAR";
             // 
-            // gridControl1
+            // gridControlStocks
             // 
-            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gridControl1.Location = new System.Drawing.Point(2, 23);
-            this.gridControl1.MainView = this.gridView1;
-            this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(303, 240);
-            this.gridControl1.TabIndex = 1000;
-            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridControlStocks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControlStocks.Location = new System.Drawing.Point(2, 23);
+            this.gridControlStocks.MainView = this.gridView1;
+            this.gridControlStocks.Name = "gridControlStocks";
+            this.gridControlStocks.Size = new System.Drawing.Size(303, 240);
+            this.gridControlStocks.TabIndex = 1000;
+            this.gridControlStocks.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
             // 
             // gridView1
             // 
             this.gridView1.Appearance.Row.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.gridView1.Appearance.Row.Options.UseBackColor = true;
-            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.GridControl = this.gridControlStocks;
             this.gridView1.Name = "gridView1";
             this.gridView1.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.False;
             this.gridView1.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.False;
@@ -99,29 +101,29 @@
             // 
             // groupControl2
             // 
-            this.groupControl2.Controls.Add(this.gridControl2);
+            this.groupControl2.Controls.Add(this.gridControlFihrist);
             this.groupControl2.Location = new System.Drawing.Point(12, 12);
             this.groupControl2.Name = "groupControl2";
             this.groupControl2.Size = new System.Drawing.Size(307, 265);
             this.groupControl2.TabIndex = 1;
-            this.groupControl2.Text = "SON 10 HAREKET";
+            this.groupControl2.Text = "FİHRİST";
             // 
-            // gridControl2
+            // gridControlFihrist
             // 
-            this.gridControl2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gridControl2.Location = new System.Drawing.Point(2, 23);
-            this.gridControl2.MainView = this.gridView2;
-            this.gridControl2.Name = "gridControl2";
-            this.gridControl2.Size = new System.Drawing.Size(303, 240);
-            this.gridControl2.TabIndex = 1000;
-            this.gridControl2.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridControlFihrist.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControlFihrist.Location = new System.Drawing.Point(2, 23);
+            this.gridControlFihrist.MainView = this.gridView2;
+            this.gridControlFihrist.Name = "gridControlFihrist";
+            this.gridControlFihrist.Size = new System.Drawing.Size(303, 240);
+            this.gridControlFihrist.TabIndex = 1000;
+            this.gridControlFihrist.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView2});
             // 
             // gridView2
             // 
             this.gridView2.Appearance.Row.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.gridView2.Appearance.Row.Options.UseBackColor = true;
-            this.gridView2.GridControl = this.gridControl2;
+            this.gridView2.GridControl = this.gridControlFihrist;
             this.gridView2.Name = "gridView2";
             this.gridView2.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.False;
             this.gridView2.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.False;
@@ -130,29 +132,31 @@
             // 
             // groupControl3
             // 
-            this.groupControl3.Controls.Add(this.gridControl3);
+            this.groupControl3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupControl3.Controls.Add(this.gridControlCompMovements);
             this.groupControl3.Location = new System.Drawing.Point(325, 13);
             this.groupControl3.Name = "groupControl3";
             this.groupControl3.Size = new System.Drawing.Size(305, 265);
             this.groupControl3.TabIndex = 3;
-            this.groupControl3.Text = "EN ÇOK İŞ YAPILAN 10 FİRMA";
+            this.groupControl3.Text = "SON 10 HAREKET";
             // 
-            // gridControl3
+            // gridControlCompMovements
             // 
-            this.gridControl3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gridControl3.Location = new System.Drawing.Point(2, 23);
-            this.gridControl3.MainView = this.gridView3;
-            this.gridControl3.Name = "gridControl3";
-            this.gridControl3.Size = new System.Drawing.Size(301, 240);
-            this.gridControl3.TabIndex = 1000;
-            this.gridControl3.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridControlCompMovements.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControlCompMovements.Location = new System.Drawing.Point(2, 23);
+            this.gridControlCompMovements.MainView = this.gridView3;
+            this.gridControlCompMovements.Name = "gridControlCompMovements";
+            this.gridControlCompMovements.Size = new System.Drawing.Size(301, 240);
+            this.gridControlCompMovements.TabIndex = 1000;
+            this.gridControlCompMovements.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView3});
             // 
             // gridView3
             // 
             this.gridView3.Appearance.Row.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.gridView3.Appearance.Row.Options.UseBackColor = true;
-            this.gridView3.GridControl = this.gridControl3;
+            this.gridView3.GridControl = this.gridControlCompMovements;
             this.gridView3.Name = "gridView3";
             this.gridView3.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.False;
             this.gridView3.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.False;
@@ -161,31 +165,32 @@
             // 
             // groupControl4
             // 
-            this.groupControl4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.groupControl4.Controls.Add(this.gridControl4);
+            this.groupControl4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupControl4.Controls.Add(this.gridControlAgenda);
             this.groupControl4.Location = new System.Drawing.Point(325, 284);
             this.groupControl4.Name = "groupControl4";
             this.groupControl4.Size = new System.Drawing.Size(307, 265);
             this.groupControl4.TabIndex = 2;
             this.groupControl4.Text = "AJANDA";
             // 
-            // gridControl4
+            // gridControlAgenda
             // 
-            this.gridControl4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gridControl4.Location = new System.Drawing.Point(2, 23);
-            this.gridControl4.MainView = this.gridView4;
-            this.gridControl4.Name = "gridControl4";
-            this.gridControl4.Size = new System.Drawing.Size(303, 240);
-            this.gridControl4.TabIndex = 1000;
-            this.gridControl4.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridControlAgenda.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControlAgenda.Location = new System.Drawing.Point(2, 23);
+            this.gridControlAgenda.MainView = this.gridView4;
+            this.gridControlAgenda.Name = "gridControlAgenda";
+            this.gridControlAgenda.Size = new System.Drawing.Size(303, 240);
+            this.gridControlAgenda.TabIndex = 1000;
+            this.gridControlAgenda.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView4});
             // 
             // gridView4
             // 
             this.gridView4.Appearance.Row.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.gridView4.Appearance.Row.Options.UseBackColor = true;
-            this.gridView4.GridControl = this.gridControl4;
+            this.gridView4.GridControl = this.gridControlAgenda;
             this.gridView4.Name = "gridView4";
             this.gridView4.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.False;
             this.gridView4.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.False;
@@ -207,6 +212,7 @@
             // 
             // xtraTabPage1
             // 
+            this.xtraTabPage1.Controls.Add(this.webBrowser1);
             this.xtraTabPage1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("xtraTabPage1.ImageOptions.Image")));
             this.xtraTabPage1.Name = "xtraTabPage1";
             this.xtraTabPage1.Size = new System.Drawing.Size(350, 509);
@@ -216,8 +222,17 @@
             // 
             this.xtraTabPage2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("xtraTabPage2.ImageOptions.Image")));
             this.xtraTabPage2.Name = "xtraTabPage2";
-            this.xtraTabPage2.Size = new System.Drawing.Size(298, 509);
+            this.xtraTabPage2.Size = new System.Drawing.Size(350, 509);
             this.xtraTabPage2.Text = "HABERLER";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(350, 509);
+            this.webBrowser1.TabIndex = 0;
             // 
             // FrmQuickGlance
             // 
@@ -232,24 +247,26 @@
             this.Name = "FrmQuickGlance";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hızlı Bakış";
+            this.Load += new System.EventHandler(this.FrmQuickGlance_Load);
             ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
             this.groupControl1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlStocks)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl2)).EndInit();
             this.groupControl2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlFihrist)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl3)).EndInit();
             this.groupControl3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlCompMovements)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl4)).EndInit();
             this.groupControl4.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlAgenda)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).EndInit();
             this.xtraTabControl1.ResumeLayout(false);
+            this.xtraTabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -257,19 +274,20 @@
         #endregion
 
         private DevExpress.XtraEditors.GroupControl groupControl1;
-        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.GridControl gridControlStocks;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
         private DevExpress.XtraEditors.GroupControl groupControl2;
-        private DevExpress.XtraGrid.GridControl gridControl2;
+        private DevExpress.XtraGrid.GridControl gridControlFihrist;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView2;
         private DevExpress.XtraEditors.GroupControl groupControl3;
-        private DevExpress.XtraGrid.GridControl gridControl3;
+        private DevExpress.XtraGrid.GridControl gridControlCompMovements;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView3;
         private DevExpress.XtraEditors.GroupControl groupControl4;
-        private DevExpress.XtraGrid.GridControl gridControl4;
+        private DevExpress.XtraGrid.GridControl gridControlAgenda;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView4;
         private DevExpress.XtraTab.XtraTabControl xtraTabControl1;
         private DevExpress.XtraTab.XtraTabPage xtraTabPage1;
         private DevExpress.XtraTab.XtraTabPage xtraTabPage2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
--- Ticari_Otomasyon/FrmQuickGlance.cs
+++ Ticari_Otomasyon/FrmQuickGlance.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Ticari_Otomasyon
@@ -15,6 +10,50 @@ namespace Ticari_Otomasyon
         public FrmQuickGlance()
         {
             InitializeComponent();
+        }
+
+        SqlConn con = new SqlConn();
+
+        void Stocks()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select name as 'Ürün Adı', sum(piece) as 'Stok Adeti' from tbl_products group by name having sum(piece)<=20 order by sum(piece)", con.connection());
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gridControlStocks.DataSource = dt;
+        }
+
+        void Agenda()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select date as 'Tarih', hour as 'Saat', title as 'Başlık', detail 'Detay', creator as 'Oluşturan', appeal as 'Hitap' from tbl_notes", con.connection());
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gridControlAgenda.DataSource = dt;
+        }
+
+        void CompMovements()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("exec CompMovements2", con.connection());
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gridControlCompMovements.DataSource = dt;
+        }
+
+        void Fihrist()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select name as 'Firma Adı', tel1 as 'Telefon' from tbl_companies", con.connection());
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gridControlFihrist.DataSource = dt;
+        }
+
+        private void FrmQuickGlance_Load(object sender, EventArgs e)
+        {
+            Stocks();
+            Agenda();
+            CompMovements();
+            Fihrist();
+
+            webBrowser1.Navigate("http://www.tcmb.gov.tr/kurlar/today.xml");
         }
     }
 }
